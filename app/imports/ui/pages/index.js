f2a9e73af209d364229276861d043c6c6064271e
matthew-schultz <mschultz@hawaii.edu>
2016-10-16 16:12:03 -1000

    Revert "Revert "broken garbage commit""
    This reverts commit 1d0d7790e472a58ea1ba3e2e19eba1e4ce216881.

--- app/imports/ui/pages/index.js
+++ app/imports/ui/pages/index.js
@@ -11,6 +11,8 @@ import './edit-contact-page.html';
 import './edit-contact-page.js';
 
 import './home-page.html';
+import './home-page.js';
 
 import './list-stuff-page.html';
 import './list-stuff-page.js';
+
